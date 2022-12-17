60c7cc095ff97d94689cbf7e4ee0fc39fe86fada
Andrej Lukasevic <forandrej@gmail.com>
2022-12-17 20:03:26 +0000

    created authentication page component alongside sign in form

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,23 +1,23 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
 
-import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
-import './navigation.style.scss'
+import "./navigation.style.scss";
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className="navigation">
-       <Link className="logo-container" to='/'>
-       <CrwnLogo className="logo" />
-       </Link>
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
+        </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
             SHOP
           </Link>
-          <Link className="nav-link" to="/sign-in">
-            SignIn
+          <Link className="nav-link" to="/auth">
+            Sign In
           </Link>
         </div>
       </div>
