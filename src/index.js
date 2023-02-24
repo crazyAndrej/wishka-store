fd77afd265744908ebfb50c73be5ddbc0d9c1bd9
Andrej Lukasevic <forandrej@gmail.com>
2023-02-24 17:18:40 +0000

    adding required PWA files and packages, updating images for manifest, add some media css

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { Elements } from "@stripe/react-stripe-js";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from '@stripe/react-stripe-js';
 
-import App from "./App";
-import { store, persistor } from "./store/store";
-import { stripePromise } from "./utils/stripe/stripe.utils";
-
-import "./index.scss";
+import App from './App';
+import { store, persistor } from './store/store';
+import { stripePromise } from './utils/stripe/stripe.utils';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 // import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -28,6 +27,7 @@ root.render(
   </React.StrictMode>
 );
 
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
